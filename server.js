e3840856fbe556eae9952e75397ed02b2128c796
Alexander Deschamps <alexadeschamps@gmail.com>
2015-01-20 17:27:05 -0500

    initial commit, working server:

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
+// var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var thoughts = ["This API is awesome", "I like eggs"];
+
+app.get('/info', function(req, res){
+	res.render("index.ejs",{});
+});
+
+app.get('/', function( req, res){
+	var rand = Math.floor(Math.random() * thoughts.length)
+	var thought = thoughts[rand];
+	var response = {
+		thought: thought
+	}
+	res.send(response);
+});
+
+app.post('/', function(req, res){
+	var data = req.body.thought;
+	thoughts.push(data);
+});
+
+app.listen(3000);
