c3abf001817b1e39f9486109463724261d7dd177
Alexander Deschamps <alexadeschamps@gmail.com>
2015-01-23 12:44:16 -0500

    first launch

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var ejs = require('ejs');
-// var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-// app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser());
 
 var thoughts = ["This API is awesome", "I like eggs"];
